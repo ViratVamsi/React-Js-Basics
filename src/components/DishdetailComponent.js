1d36715fde984b17922ab6455e1dfe8ade713fd7
vamsi <virat.vamsi58@gmail.com>
2020-07-19 11:06:25 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,72 +1,65 @@
-import React, {Component} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
-class DishdetailComponent extends Component{
-    
-    constructor(props){
-        super(props);
-    }
-
-    renderDish(dish){
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg width="100%" top src={dish.image}></CardImg>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
-
-    renderComments(commentsList){
-        const cmnts = commentsList.map(comment => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
-                    </p>
-                </li>
-            )
-        })
-        return(
-            
-            <ul className='list-unstyled'>
-                {cmnts}
-            </ul>
-        );
-    }
-
-    render(){
-
+const DishDetail = (props) => {
+    if(props.dish!=null){
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                        <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <h4> Comments </h4>
-                        {this.props.dish && this.renderComments(this.props.dish.comments)}
+                        
+                        < RenderComments comments={props.dish.comments} />
                     </div>
                 </div>
             </div>
         );
     }
-
+    return(
+        <div></div>
+    );
 }
 
 
-export default DishdetailComponent;
+function RenderDish({dish}){
+    return(
+        <Card>
+            <CardImg width="100%" top src={dish.image}></CardImg>
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+function RenderComments({comments}){
+    const cmnts = comments.map(comment => {
+        return (
+            <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author},
+                &nbsp;
+                {new Intl.DateTimeFormat('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: '2-digit'
+                    }).format(new Date(comment.date))}
+                </p>
+            </li>
+        )
+    })
+    return(
+        
+        <ul className='list-unstyled'>
+            {cmnts}
+        </ul>
+    );
+}
+
+
+export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
-import DishdetailComponent from './DishdetailComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -27,7 +27,8 @@ class Main extends Component {
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishdetailComponent dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        console.log(this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0])
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
       </div>
     );
   }
